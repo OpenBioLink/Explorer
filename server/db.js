fd8d175a8db59c407ffc0b9a77a5b0bd4d61a4c9
simon <simonott@gmx.at>
2021-04-19 16:07:22 +0200

    added autocreate index if none present

--- server/db.js
+++ server/db.js
@@ -3,11 +3,59 @@
 const Database = require('better-sqlite3');
 const fs = require('fs');
 
+function createIndex(){
+    let db = new Database(`./db/index.db`, { verbose: console.log });
+
+    sql = `
+    CREATE TABLE "Dataset" (
+        "ID"	TEXT NOT NULL,
+        "Name"	TEXT NOT NULL,
+        "Version"	TEXT,
+        "Description"	TEXT,
+        "Namespace"	TEXT NOT NULL,
+        PRIMARY KEY("ID")
+    );`
+    db.prepare(sql).run(sql);
+    sql = `
+    CREATE TABLE "Explaination" (
+        "ID"	TEXT NOT NULL,
+        "DatasetID"	TEXT NOT NULL,
+        "Label"	TEXT,
+        "Date"	INTEGER NOT NULL,
+        "Comment"	TEXT,
+        "Method"	TEXT NOT NULL,
+        "RuleConfig"	TEXT,
+        "ClusteringConfig"	TEXT,
+        PRIMARY KEY("ID"),
+        FOREIGN KEY("DatasetID") REFERENCES "Dataset"("ID")
+    );`
+    db.prepare(sql).run(sql);
+    sql = `
+    CREATE TABLE "Temp_Dataset" (
+        "ID"	TEXT NOT NULL,
+        "Namespace"	TEXT NOT NULL,
+        "Date"	INTEGER NOT NULL,
+        PRIMARY KEY("ID")
+    );
+    `
+    db.prepare(sql).run(sql);
+    sql = `
+    CREATE TABLE "Temp_Explaination" (
+        "ID"	TEXT NOT NULL,
+        "DatasetID"	TEXT NOT NULL,
+        "Date"	INTEGER NOT NULL,
+        PRIMARY KEY("ID")
+    );`
+    db.prepare(sql).run(sql);
+    return db;
+}
+
 let queries = {
     connectToDB(db_id){
         if(fs.existsSync(`./db/${db_id}.db`)){
-            let db = new Database(`./db/${db_id}.db`, { verbose: console.log });
-            return db;
+            return new Database(`./db/${db_id}.db`, { verbose: console.log });
+        } else if(db_id == 'index'){
+            return createIndex();
         }
     },
     all(db_id, sql){
